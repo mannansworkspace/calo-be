057af2a43f65c579d76c30d8c7d904fdf5620da2
mannansmannansworkspace <mannansworkspace@gmail.com>
2024-10-21 21:08:08 +0500

    swagger docs, cors, globalPipe added

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
@@ -6,17 +6,22 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 const PORT = 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: true,
+  });
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Calo Challenge')
     .setDescription('This is API documentation for Calo Challenge')
     .setVersion('1.0')
+    .addTag('JOBS')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  await app.listen(PORT);
   Logger.log(`Server is listening at port ${PORT}`);
 }
 bootstrap();

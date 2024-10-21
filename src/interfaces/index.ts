5407e83fea5127c2fced5a42308d72ea5dcae71f
mannansmannansworkspace <mannansworkspace@gmail.com>
2024-10-21 21:08:44 +0500

    constants and interfaces added

---
+++ src/interfaces/index.ts
@@ -0,0 +1,33 @@
+import { ExperienceLevel, JobStatus, JobType } from 'src/constants';
+
+export interface TimeStamps {
+  createdAt: Date;
+  deleteAt?: Date;
+  updatedAt: Date;
+}
+
+export interface JobImageUrls {
+  urls: {
+    raw: string;
+    full: string;
+    regular: string;
+    small: string;
+    thumb: string;
+    small_s3: string;
+  } | null;
+}
+
+export interface AppJob extends TimeStamps, JobImageUrls {
+  id: string;
+  title: String;
+  description: String;
+  jobType: JobType;
+  experienceLevel: ExperienceLevel;
+  status: JobStatus;
+}
+
+export interface Generic<K, V> {
+  K: V;
+}
+
+export type Jobs = Generic<string, AppJob>;
